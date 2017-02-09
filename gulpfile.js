43fa916ec731531de2c939f646fd9bac40e36059
Christophe Remacle <Christophe Remacle>
2017-02-09 11:17:47 +0100

    Define gulp env

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/* KatBizniss
+ *
+ * Gulp tasks
+ *
+ * coded by Gotrixx
+ * started at 09/02/2017
+ */
+
+var gulp = require("gulp");
+
+// --- Task for images
+
+// --- Task for styles
+
+// --- Task for pug
+
+// --- Task for js
+
+// --- Watch tasks
+
+// --- Aliases
